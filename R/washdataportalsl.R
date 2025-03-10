2e33b97f1693fefab6f3c3c9ec67e5262ce54a4c
Lars Schöbitz <lschoebitz@ethz.ch>
2025-03-10 16:14:51 +0100

    prepare data documentation

---
+++ R/washdataportalsl.R
@@ -0,0 +1,13 @@
+#' washdataportalsl: WASH data portal of Sierra Leone
+#'
+#' The dataset contains information on water points in Sierra Leone. The data is
+#' sourced from the Sierra Leone WASH data portal: https://washdata-sl.org/.
+#'
+#' @format A tibble with 31451 rows and 4 variables
+#' \describe{
+#'   \item{district}{ District of the water point location.}
+#'   \item{chiefdom}{ Chiefdom of the water point location.}
+#'   \item{section}{ Section of the water point location.}
+#'   \item{type_of_water_point}{ Type of water point.}
+#' }
+"washdataportalsl"
